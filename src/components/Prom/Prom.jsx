995dfb861d705d669462e36a5b727ef42459445a
norma <normanmagic2004@gmail.com>
2024-07-09 11:54:13 +0300

    promo

---
+++ src/components/Prom/Prom.jsx
@@ -0,0 +1,28 @@
+import { Component } from "react";
+import { Container } from "react-bootstrap";
+
+import "./__prom.scss";
+
+export default class Prom extends Component {
+  render() {
+    return (
+      <section className="prom">
+        <Container className="prom__container">
+          <h1 className="promo_title">
+            All <span> the sneakers </span> You want are here
+          </h1>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur. Lorem sit aenean erat
+            tincidunt nulla potenti vulputate.{" "}
+          </p>
+
+          <div className="prom__buttons">
+            <button className="prom__shopBtn">SHOP NOW</button>
+            <button className="prom__collectiobBtn">ALL COLLECTIONS</button>
+          </div>
+        </Container>
+      </section>
+    );
+  }
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./index.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header/Header";
+import Prom from "./components/Prom/Prom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Header />
+    <Prom />
   </React.StrictMode>
 );
