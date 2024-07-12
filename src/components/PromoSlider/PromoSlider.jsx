79fd0ba67cddb78e1bb7539467966fab9bd1c8ca
norma <normanmagic2004@gmail.com>
2024-07-12 10:49:27 +0300

    slider

--- src/components/PromoSlider/PromoSlider.jsx
+++ src/components/PromoSlider/PromoSlider.jsx
@@ -4,9 +4,10 @@ import "./__promoslider.scss";
 
 export default class PromoSlider extends Component {
   render() {
-    return <section className="promoslider">
-
-
-    </section>;
+    return (
+      <section className="promoslider">
+        <span className="promoslider__text">jUST DO IT</span>
+      </section>
+    );
   }
 }
