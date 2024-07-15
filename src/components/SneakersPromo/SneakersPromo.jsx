b9299771faa6fde966b179876d295da0c4ec60c8
norma <normanmagic2004@gmail.com>
2024-07-15 11:15:49 +0300

    optimize

--- src/components/SneakersPromo/SneakersPromo.jsx
+++ src/components/SneakersPromo/SneakersPromo.jsx
@@ -1,11 +1,10 @@
 import React, { Component } from "react";
 import { Container, Row } from "react-bootstrap";
-
 import "./__SneakersPromo.scss";
-
 import { sneakers } from "./data";
-
 import SneakerCard from "./SneakersCard";
+
+const brands = ["air jordan", "nike", "dior", "zara", "adidas"];
 
 export default class SneakersPromo extends Component {
   constructor(props) {
@@ -35,45 +34,22 @@ export default class SneakersPromo extends Component {
           <div className="sneaker__text">
             <h1>Browse our best collections</h1>
             <p>
+              {" "}
               Lorem ipsum dolor sit amet consectetur. Est eros in commodo
               pellentesque neque tempus imperdiet enim a. Sit morbi convallis
               suscipit vitae lacus vitae id urna pharetra.
             </p>
-
             <nav className="sneaker__nav">
               <ol>
-                <li
-                  className={`sneaker__li ${
-                    activeBrand === "air jordan" ? "active" : ""
-                  }`}
-                  onClick={() => this.filterSneakers("air jordan")}
-                >
-                  air jordan
-                </li>
-                <li
-                  className={`${activeBrand === "nike" ? "active" : ""}`}
-                  onClick={() => this.filterSneakers("nike")}
-                >
-                  nike
-                </li>
-                <li
-                  className={`${activeBrand === "dior" ? "active" : ""}`}
-                  onClick={() => this.filterSneakers("dior")}
-                >
-                  dior
-                </li>
-                <li
-                  className={`${activeBrand === "zara" ? "active" : ""}`}
-                  onClick={() => this.filterSneakers("zara")}
-                >
-                  zara
-                </li>
-                <li
-                  className={`${activeBrand === "adidas" ? "active" : ""}`}
-                  onClick={() => this.filterSneakers("adidas")}
-                >
-                  adidas
-                </li>
+                {brands.map((brand) => (
+                  <li
+                    key={brand}
+                    className={`${activeBrand === brand ? "active" : ""}`}
+                    onClick={() => this.filterSneakers(brand)}
+                  >
+                    {brand}
+                  </li>
+                ))}
               </ol>
             </nav>
           </div>
@@ -86,7 +62,6 @@ export default class SneakersPromo extends Component {
             <button className="sneaker__btn">View all</button>
           </div>
         </Container>
-            
       </section>
     );
   }
