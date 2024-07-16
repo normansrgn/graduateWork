c45e00fc83f5b4be574fb4af8516d8d5f111e76f
norma <normanmagic2004@gmail.com>
2024-07-16 15:30:01 +0300

    README file

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,9 +3,9 @@ import { Container } from "react-bootstrap";
 
 import "./__header.scss";
 import HeaderNav from "./HeaderNav";
+import BurgerMenu from "../BurgerMenu/BurgerMenu";
 
 import logo from "./logo.svg";
-import BurgerMenu from "../BurgerMenu/BurgerMenu";
 
 export default class Header extends Component {
   render() {
