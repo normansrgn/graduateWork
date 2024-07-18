dbd0bf8e48cfbadd8cc4096822bbfa30e42bbe56
norma <normanmagic2004@gmail.com>
2024-07-18 09:42:47 +0300

    redisign header nav

--- src/components/Header/HeaderNav.jsx
+++ src/components/Header/HeaderNav.jsx
@@ -1,3 +1,6 @@
+import user from "./NavICons/user.svg";
+import basket from "./NavICons/basket.svg";
+
 function HeaderNav() {
   return (
     <div className="header__navBlock">
@@ -9,8 +12,13 @@ function HeaderNav() {
         </ol>
       </nav>
 
-      <div className="header__form">
-        <input type="text" placeholder="Search" />
+      <div className="header__navIc">
+        <a href="">
+          <img src={user} alt="" />
+        </a>
+        <a href="">
+          <img src={basket} alt="" />
+        </a>
       </div>
     </div>
   );
