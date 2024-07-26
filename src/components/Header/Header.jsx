a07a6073185e4848de6f3da056d2e5d4e57e0092
norma <normanmagic2004@gmail.com>
2024-07-26 09:04:00 +0300

    $

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,6 @@ import HeaderNav from "./HeaderNav";
 import BurgerMenu from "../BurgerMenu/BurgerMenu";
 
 import logo from "./logo.svg";
-
 export default class Header extends Component {
   render() {
     return (
