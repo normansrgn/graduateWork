43d6b46f11db505f946fb1409adb0bb515549da3
norma <normanmagic2004@gmail.com>
2024-07-28 15:57:33 +0300

    routing

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+// import React from "react";
+// import Header from "../components/Header/Header";
+
+// import Prom from "../components/Prom/Prom";
+// import MenWomen from "../components/MenWomen/MenWomen";
+// import SneakersPromo from "../components/SneakersPromo/SneakersPromo";
+// import PromoSlider from "../components/PromoSlider/PromoSlider";
+
+// function Home() {
+//   return (
+//     <>
+//       <Prom />
+//       <MenWomen />
+//       <SneakersPromo />
+//       <PromoSlider />
+//     </>
+//   );
+// }
+
+// export default Home;
