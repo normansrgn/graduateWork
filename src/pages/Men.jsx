ed34705ee5f868ba3b5e77c87c36fb0882e58f7f
norma <normanmagic2004@gmail.com>
2024-07-30 09:15:18 +0300

    $

--- src/pages/Men.jsx
+++ src/pages/Men.jsx
@@ -8,14 +8,14 @@ import MenPage from "../components/MenSneakerPage/MenSneakerPage";
 export default class Men extends Component {
   render() {
     return (
-      <React.Fragment>
+      <>
         <Container>
           <div className="men">
             <MenPage />
             <SneakersPromoMen />
           </div>
         </Container>
-      </React.Fragment>
+      </>
     );
   }
 }
