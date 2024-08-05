aab8374b53130abe645574b601bd350641a63f0d
norma <normanmagic2004@gmail.com>
2024-08-05 20:33:52 +0300

    semantic

--- src/components/SneakersPromo/SneakersPromo.jsx
+++ src/components/SneakersPromo/SneakersPromo.jsx
@@ -17,16 +17,16 @@ export default class SneakersPromo extends Component {
         sneaker.title.toLowerCase().includes(initialBrand)
       ),
       activeBrand: initialBrand,
-      isMobile: window.innerWidth <= 768
+      isMobile: window.innerWidth <= 768,
     };
   }
 
   componentDidMount() {
-    window.addEventListener('resize', this.handleResize);
+    window.addEventListener("resize", this.handleResize);
   }
 
   componentWillUnmount() {
-    window.removeEventListener('resize', this.handleResize);
+    window.removeEventListener("resize", this.handleResize);
   }
 
   handleResize = () => {
@@ -49,53 +49,55 @@ export default class SneakersPromo extends Component {
     const { activeBrand, filteredSneakers, isMobile } = this.state;
 
     return (
-      <section className="sneaker">
-        <Container className="sneaker__container">
-          <div className="sneaker__text">
-            <h1>Browse our best collections</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur. Est eros in commodo
-              pellentesque neque tempus imperdiet enim a. Sit morbi convallis
-              suscipit vitae lacus vitae id urna pharetra.
-            </p>
-            {isMobile ? (
-              <select
-                className="sneaker__select"
-                value={activeBrand}
-                onChange={this.handleSelectChange}
-              >
-                {brands.map((brand) => (
-                  <option key={brand} value={brand}>
-                    {brand}
-                  </option>
-                ))}
-              </select>
-            ) : (
-              <nav className="sneaker__nav">
-                <ol>
+      <>
+        <section className="sneaker">
+          <Container className="sneaker__container">
+            <div className="sneaker__text">
+              <h1>Browse our best collections</h1>
+              <p>
+                Lorem ipsum dolor sit amet consectetur. Est eros in commodo
+                pellentesque neque tempus imperdiet enim a. Sit morbi convallis
+                suscipit vitae lacus vitae id urna pharetra.
+              </p>
+              {isMobile ? (
+                <select
+                  className="sneaker__select"
+                  value={activeBrand}
+                  onChange={this.handleSelectChange}
+                >
                   {brands.map((brand) => (
-                    <li
-                      key={brand}
-                      className={`${activeBrand === brand ? "active" : ""}`}
-                      onClick={() => this.filterSneakers(brand)}
-                    >
+                    <option key={brand} value={brand}>
                       {brand}
-                    </li>
+                    </option>
                   ))}
-                </ol>
-              </nav>
-            )}
-          </div>
-          <Row className="sneaker__row">
-            {filteredSneakers.slice(0, 3).map((sneaker) => (
-              <SneakerCard key={sneaker.title} {...sneaker} />
-            ))}
-          </Row>
-          <div className="sneaker__button">
-            <button className="sneaker__btn">View all</button>
-          </div>
-        </Container>
-      </section>
+                </select>
+              ) : (
+                <nav className="sneaker__nav">
+                  <ol>
+                    {brands.map((brand) => (
+                      <li
+                        key={brand}
+                        className={`${activeBrand === brand ? "active" : ""}`}
+                        onClick={() => this.filterSneakers(brand)}
+                      >
+                        {brand}
+                      </li>
+                    ))}
+                  </ol>
+                </nav>
+              )}
+            </div>
+            <Row className="sneaker__row">
+              {filteredSneakers.slice(0, 3).map((sneaker) => (
+                <SneakerCard key={sneaker.title} {...sneaker} />
+              ))}
+            </Row>
+            <div className="sneaker__button">
+              <button className="sneaker__btn">View all</button>
+            </div>
+          </Container>
+        </section>
+      </>
     );
   }
 }
