5307a8f26048c814036e5576625d28e7d51420e6
norma <normanmagic2004@gmail.com>
2024-08-12 20:35:16 +0300

    basket mobile

--- src/components/BasketItems/BasketCard.jsx
+++ src/components/BasketItems/BasketCard.jsx
@@ -5,15 +5,16 @@ import { Container } from "react-bootstrap";
 import "./__BasketCard.scss";
 import image from "../SneakersPromo/images/1.png";
 
-
 function BasketCard() {
   return (
     <>
-      <section className="basketCard">
-        <img src={image} alt="image" className="basketCard__image" />
-        <div className="basketCard__title">Air Jordan 5 Retro</div>
-        <div className="basketCard__price">$200</div>
-      </section>
+      <div className="col-xxl-6">
+        <section className="basketCard">
+          <img src={image} alt="image" className="basketCard__image" />
+          <div className="basketCard__title">Air Jordan 5 Retro</div>
+          <div className="basketCard__price">$200</div>
+        </section>
+      </div>
     </>
   );
 }
