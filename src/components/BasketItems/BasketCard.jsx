d5c554da4162b381e6ca369cda2ed26976949eac
norma <normanmagic2004@gmail.com>
2024-08-12 20:26:29 +0300

    basket card

--- src/components/BasketItems/BasketCard.jsx
+++ src/components/BasketItems/BasketCard.jsx
@@ -3,11 +3,17 @@ import React from "react";
 import { Container } from "react-bootstrap";
 
 import "./__BasketCard.scss";
+import image from "../SneakersPromo/images/1.png";
+
 
 function BasketCard() {
   return (
     <>
-      <section className="basketCard"></section>
+      <section className="basketCard">
+        <img src={image} alt="image" className="basketCard__image" />
+        <div className="basketCard__title">Air Jordan 5 Retro</div>
+        <div className="basketCard__price">$200</div>
+      </section>
     </>
   );
 }
