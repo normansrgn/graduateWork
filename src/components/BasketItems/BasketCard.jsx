37d250a876625973ae0f9969fa31bf0de74223f6
norma <normanmagic2004@gmail.com>
2024-08-13 13:08:08 +0300

    basket

--- src/components/BasketItems/BasketCard.jsx
+++ src/components/BasketItems/BasketCard.jsx
@@ -1,24 +1,54 @@
-import React from "react";
-
-import { Container } from "react-bootstrap";
-
+import React, { useState, useEffect } from "react";
 import "./__BasketCard.scss";
-import image from "../SneakersPromo/images/1.png";
 
 function BasketCard() {
+  const [cartItems, setCartItems] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  useEffect(() => {
+    const cart = JSON.parse(localStorage.getItem("cart")) || [];
+    setCartItems(cart);
+
+    const total = cart.reduce((sum, item) => sum + item.price, 0);
+    setTotalPrice(total);
+  }, []);
+
+  const removeFromCart = (indexToRemove) => {
+    const updatedCart = cartItems.filter((_, index) => index !== indexToRemove);
+    localStorage.setItem("cart", JSON.stringify(updatedCart));
+    setCartItems(updatedCart);
+
+    const total = updatedCart.reduce((sum, item) => sum + item.price, 0);
+    setTotalPrice(total);
+  };
+
   return (
     <>
-      <div className="col-xxl-6">
-        <section className="basketCard">
-          <img src={image} alt="image" className="basketCard__image" />
-          <div className="basketCard__title">Air Jordan 5 Retro</div>
-          <div className="basketCard__price">$200</div>
-        </section>
-      </div>
+      {cartItems.map((item, index) => (
+        <div key={index} className="col-xxl-6 ">
+          <section className="basketCard">
+            <img
+              src={item.img}
+              alt={item.title}
+              className="basketCard__image"
+            />
+            <div className="basketCard__title">{item.title}</div>
+            <div className="basketCard__price">
+              ${item.price}
+              <button
+                onClick={() => removeFromCart(index)}
+                className="basketCard__removeButton"
+              >
+                x
+              </button>
+            </div>
+          </section>
+        </div>
+      ))}
       <div className="col-xxl-6">
         <section className="basketCardInfo">
           <div className="basketCardInfo__title">ИТОГО:</div>
-          <div className="basketCardInfo__price">$200</div>
+          <div className="basketCardInfo__price">${totalPrice}</div>
         </section>
       </div>
       <div className="col-xxl-6">
--- src/components/WomenSneaker/SneakersPromoMen/SneakersCard.jsx
+++ src/components/WomenSneaker/SneakersPromoMen/SneakersCard.jsx
@@ -1,13 +1,26 @@
+import React from "react";
+
 function SneakerCard(props) {
+  const addToCart = () => {
+    const currentCart = JSON.parse(localStorage.getItem('cart')) || [];
+    const newItem = {
+      img: props.img,
+      title: props.title,
+      price: props.price
+    };
+    currentCart.push(newItem);
+    localStorage.setItem('cart', JSON.stringify(currentCart));
+  };
+
   return (
     <div className="col-xxl-4 col-sm-6 col-md-7">
       <div className="sneaker__card">
-        <img src={props.img} alt="" />
+        <img src={props.img} alt={props.title} />
         <div className="sneaker__cardText">
           <h2 className="sneaker__cardTitle">{props.title}</h2>
           <div className="sneaker__cardPrice">
             <span>${props.price}</span>
-            <button>BUY NOW</button>
+            <button onClick={addToCart}>BUY NOW</button>
           </div>
         </div>
       </div>
