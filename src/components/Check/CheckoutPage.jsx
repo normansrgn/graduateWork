649f097dd4842c9460a7ef32309e8f743d3a41d1
norma <normanmagic2004@gmail.com>
2024-08-27 17:16:42 +0300

    check page

--- src/components/Check/CheckoutPage.jsx
+++ src/components/Check/CheckoutPage.jsx
@@ -10,45 +10,66 @@ function CheckoutPage() {
   const { cartItems, totalPrice } = location.state;
 
   return (
-    <div className="checkoutPage">
-      <Container>
-        <h1 className="checkoutPage__title">Оформление заказа</h1>
-        {cartItems.length > 0 ? (
-          <>
-            <div className="col-xxl-6 checkoutPage__card">
-              <div className="checkoutPage__items">
-                {cartItems.map((item, index) => (
-                  <div key={index} className="checkoutPage__item">
-                    <img
-                      src={item.img}
-                      alt={item.title}
-                      className="checkoutPage__image"
-                    />
-                    <div className="checkoutPage__details">
-                      <span className="checkoutPage__itemTitle">
-                        {item.title}
-                      </span>
-                      <span className="checkoutPage__itemPrice">
-                        ${item.price}
-                      </span>
-                    </div>
-                  </div>
-                ))}
-              </div>
-              <div className="checkoutPage__total">
-                <span>Итого:</span> <span>${totalPrice}</span>
-              </div>
-            </div>
+    <>
+      <h1 className="checkoutPage__title">Оформление заказа</h1>
 
-            <button className="checkoutPage__confirmButton">
-              Перейти к оплате
-            </button>
-          </>
-        ) : (
-          <p>Ваша корзина пуста</p>
-        )}
+      <Container>
+        <h2 className="userInfo__title">Личные данные</h2>
       </Container>
-    </div>
+      <Container className="checkCont">
+        <div className="userInfo">
+          <div className="userInfo__content">
+            <form action="" className="userInfo__form">
+              <input type="text" placeholder="Имя и Фамилия" />
+              <input type="number" placeholder="Телефон" />
+              <input type="text" placeholder="Почта" />
+            </form>
+          </div>
+          <div className="userInfo__content">
+            <h2 className="userInfo__title">Адрес доставки</h2>
+            <form action="" className="userInfo__form">
+              <input type="text" placeholder="Ваш адрес" />
+            </form>
+          </div>
+        </div>
+        <div className="checkoutPage">
+          {cartItems.length > 0 ? (
+            <>
+              <div className="checkoutPage__card">
+                <div className="checkoutPage__items">
+                  {cartItems.map((item, index) => (
+                    <div key={index} className="checkoutPage__item">
+                      <img
+                        src={item.img}
+                        alt={item.title}
+                        className="checkoutPage__image"
+                      />
+                      <div className="checkoutPage__details">
+                        <span className="checkoutPage__itemTitle">
+                          {item.title}
+                        </span>
+                        <span className="checkoutPage__itemPrice">
+                          ${item.price}
+                        </span>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+                <div className="checkoutPage__total">
+                  <span>К оплате:</span> <span>${totalPrice}</span>
+                </div>
+
+                <button className="checkoutPage__confirmButton">
+                  Перейти к оплате
+                </button>
+              </div>
+            </>
+          ) : (
+            <p>Ваша корзина пуста</p>
+          )}
+        </div>
+      </Container>
+    </>
   );
 }
 
