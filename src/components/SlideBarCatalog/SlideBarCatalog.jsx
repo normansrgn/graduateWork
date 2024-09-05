5ad5207b77dd51f21fe2d9d21091dc689845337c
norma <normanmagic2004@gmail.com>
2024-09-05 17:46:03 +0300

    redisigne slidebar

--- src/components/SlideBarCatalog/SlideBarCatalog.jsx
+++ src/components/SlideBarCatalog/SlideBarCatalog.jsx
@@ -5,36 +5,81 @@ import "./__SlideBarCatalog.scss";
 export default function SlideBarCatalog() {
   return (
     <>
-      <section className="SlideBarCatalog">
-        <div className="SlideBarCatalog__brands">
-          <h1 className="SlideBarCatalog__title">Бренды</h1>
-          <div className="SlideBarCatalog__filterList">
-            <div className="SlideBarCatalog__filterItem">
-              <input
-                type="checkbox"
-                readonly
-                name="Nike"
-                className="SlideBarCatalog__filterItemChecbox"
-              />
-              <label for="nike">Nike</label>
+      <section className="SlideBarCatalog__container">
+        <div className="SlideBarCatalog">
+          <div className="SlideBarCatalog__brands">
+            <h1 className="SlideBarCatalog__title">Бренд:</h1>
+            <div className="SlideBarCatalog__filterList">
+              <div className="SlideBarCatalog__filterItem">
+                <input
+                  type="checkbox"
+                  readonly
+                  name="Nike"
+                  className="SlideBarCatalog__filterItemChecbox"
+                />
+                <label for="nike">Nike</label>
+              </div>
+              <div className="SlideBarCatalog__filterItem">
+                <input
+                  type="checkbox"
+                  readonly
+                  name="Adidas"
+                  className="SlideBarCatalog__filterItemChecbox"
+                />
+                <label for="Adidas">Adidas</label>
+              </div>
+              <div className="SlideBarCatalog__filterItem">
+                <input
+                  type="checkbox"
+                  readonly
+                  name="Puma"
+                  className="SlideBarCatalog__filterItemChecbox"
+                />
+                <label for="Puma">Puma</label>
+              </div>
             </div>
-            <div className="SlideBarCatalog__filterItem">
-              <input
-                type="checkbox"
-                readonly
-                name="Adidas"
-                className="SlideBarCatalog__filterItemChecbox"
-              />
-              <label for="Adidas">Adidas</label>
-            </div>
-            <div className="SlideBarCatalog__filterItem">
-              <input
-                type="checkbox"
-                readonly
-                name="Puma"
-                className="SlideBarCatalog__filterItemChecbox"
-              />
-              <label for="Puma">Puma</label>
+          </div>
+        </div>
+        <div className="SlideBarCatalog">
+          <div className="SlideBarCatalog__brands">
+            <h1 className="SlideBarCatalog__title">Модель:</h1>
+            <div className="SlideBarCatalog__filterList">
+              <div className="SlideBarCatalog__filterItem">
+                <input
+                  type="checkbox"
+                  readonly
+                  name="Jordan"
+                  className="SlideBarCatalog__filterItemChecbox"
+                />
+                <label for="Jordan">Jordan</label>
+              </div>
+              <div className="SlideBarCatalog__filterItem">
+                <input
+                  type="checkbox"
+                  readonly
+                  name="Dunk"
+                  className="SlideBarCatalog__filterItemChecbox"
+                />
+                <label for="Dunk">Dunk</label>
+              </div>
+              <div className="SlideBarCatalog__filterItem">
+                <input
+                  type="checkbox"
+                  readonly
+                  name="Air Max"
+                  className="SlideBarCatalog__filterItemChecbox"
+                />
+                <label for="Air Max">Air Max</label>
+              </div>
+              <div className="SlideBarCatalog__filterItem">
+                <input
+                  type="checkbox"
+                  readonly
+                  name="Air Max"
+                  className="SlideBarCatalog__filterItemChecbox"
+                />
+                <label for="Air Max">Air Max</label>
+              </div>
             </div>
           </div>
         </div>
