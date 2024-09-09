cd397924217f95ee88487e3cfc062e9cb5427824
norma <normanmagic2004@gmail.com>
2024-09-09 19:49:34 +0300

    $

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
-
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Router basename="">
+  <Router basename="/">
     <App />
   </Router>
 );
