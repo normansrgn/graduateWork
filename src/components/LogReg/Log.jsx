089b8b3ad80e3bc272572405a97270822b9f4414
norma <normanmagic2004@gmail.com>
2024-09-13 21:35:52 +0300

    restyleing log/reg pages

--- src/components/LogReg/Log.jsx
+++ src/components/LogReg/Log.jsx
@@ -11,16 +11,42 @@ function Login() {
         <div className="login__container">
           <h1 className="login__title">Вход</h1>
           <form action="" className="login__form">
-            <input
-              type="tel"
-              className="login__input"
-              placeholder="Номер телефона"
-            />
-            <input
-              type="password"
-              className="login__input"
-              placeholder="Пароль"
-            />
+            <div className="login__input">
+              <i className="login__icon">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  width="28"
+                  height="28"
+                  fill="rgba(255,254,254,1)"
+                >
+                  <path d="M3 6H21V18H3V6ZM2 4C1.44772 4 1 4.44772 1 5V19C1 19.5523 1.44772 20 2 20H22C22.5523 20 23 19.5523 23 19V5C23 4.44772 22.5523 4 22 4H2ZM13 8H19V10H13V8ZM18 12H13V14H18V12ZM10.5 10C10.5 11.3807 9.38071 12.5 8 12.5C6.61929 12.5 5.5 11.3807 5.5 10C5.5 8.61929 6.61929 7.5 8 7.5C9.38071 7.5 10.5 8.61929 10.5 10ZM8 13.5C6.067 13.5 4.5 15.067 4.5 17H11.5C11.5 15.067 9.933 13.5 8 13.5Z"></path>
+                </svg>
+              </i>
+              <input
+                type="tel"
+                className="login__input"
+                placeholder="Номер телефона"
+              />
+            </div>
+            <div className="login__input">
+              <i className="login__icon">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  width="28"
+                  height="28"
+                  fill="rgba(255,255,255,1)"
+                >
+                  <path d="M6 8V7C6 3.68629 8.68629 1 12 1C15.3137 1 18 3.68629 18 7V8H20C20.5523 8 21 8.44772 21 9V21C21 21.5523 20.5523 22 20 22H4C3.44772 22 3 21.5523 3 21V9C3 8.44772 3.44772 8 4 8H6ZM19 10H5V20H19V10ZM11 15.7324C10.4022 15.3866 10 14.7403 10 14C10 12.8954 10.8954 12 12 12C13.1046 12 14 12.8954 14 14C14 14.7403 13.5978 15.3866 13 15.7324V18H11V15.7324ZM8 8H16V7C16 4.79086 14.2091 3 12 3C9.79086 3 8 4.79086 8 7V8Z"></path>
+                </svg>
+              </i>
+              <input
+                type="password"
+                className="login__input"
+                placeholder="Пароль"
+              />
+            </div>
             <div className="login__button">
               <Link to="/">
                 <button>Войти</button>
--- src/components/LogReg/Reg.jsx
+++ src/components/LogReg/Reg.jsx
@@ -11,18 +11,78 @@ function Registr() {
         <Container className="login__container">
           <h1 className="reg__title">Регистрация</h1>
           <form action="" className="login__form">
-            <input type="text" className="login__input" placeholder="Имя" />
-            <input type="number" className="login__input" placeholder="Номер телефона" />
-            <input
-              type="password"
-              className="login__input"
-              placeholder="Придумайте пароль"
-            />
-             <input
-              type="password"
-              className="login__input"
-              placeholder="Повотрите пароль"
-            />
+            <div className="login__input">
+              <i className="login__icon">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  width="28"
+                  height="28"
+                  fill="rgba(255,254,254,1)"
+                >
+                  <path d="M3 6H21V18H3V6ZM2 4C1.44772 4 1 4.44772 1 5V19C1 19.5523 1.44772 20 2 20H22C22.5523 20 23 19.5523 23 19V5C23 4.44772 22.5523 4 22 4H2ZM13 8H19V10H13V8ZM18 12H13V14H18V12ZM10.5 10C10.5 11.3807 9.38071 12.5 8 12.5C6.61929 12.5 5.5 11.3807 5.5 10C5.5 8.61929 6.61929 7.5 8 7.5C9.38071 7.5 10.5 8.61929 10.5 10ZM8 13.5C6.067 13.5 4.5 15.067 4.5 17H11.5C11.5 15.067 9.933 13.5 8 13.5Z"></path>
+                </svg>
+              </i>
+              <input
+                type="tel"
+                className="login__input"
+                placeholder="Ваше Имя"
+              />
+            </div>
+            <div className="login__input">
+              <i className="login__icon">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  width="28"
+                  height="28"
+                  fill="rgba(255,255,255,1)"
+                >
+                  <path d="M9.36556 10.6821C10.302 12.3288 11.6712 13.698 13.3179 14.6344L14.2024 13.3961C14.4965 12.9845 15.0516 12.8573 15.4956 13.0998C16.9024 13.8683 18.4571 14.3353 20.0789 14.4637C20.599 14.5049 21 14.9389 21 15.4606V19.9234C21 20.4361 20.6122 20.8657 20.1022 20.9181C19.5723 20.9726 19.0377 21 18.5 21C9.93959 21 3 14.0604 3 5.5C3 4.96227 3.02742 4.42771 3.08189 3.89776C3.1343 3.38775 3.56394 3 4.07665 3H8.53942C9.0611 3 9.49513 3.40104 9.5363 3.92109C9.66467 5.54288 10.1317 7.09764 10.9002 8.50444C11.1427 8.9484 11.0155 9.50354 10.6039 9.79757L9.36556 10.6821ZM6.84425 10.0252L8.7442 8.66809C8.20547 7.50514 7.83628 6.27183 7.64727 5H5.00907C5.00303 5.16632 5 5.333 5 5.5C5 12.9558 11.0442 19 18.5 19C18.667 19 18.8337 18.997 19 18.9909V16.3527C17.7282 16.1637 16.4949 15.7945 15.3319 15.2558L13.9748 17.1558C13.4258 16.9425 12.8956 16.6915 12.3874 16.4061L12.3293 16.373C10.3697 15.2587 8.74134 13.6303 7.627 11.6707L7.59394 11.6126C7.30849 11.1044 7.05754 10.5742 6.84425 10.0252Z"></path>
+                </svg>
+              </i>
+              <input
+                type="nu,mber"
+                className="login__input"
+                placeholder="Номер телефона"
+              />
+            </div>
+            <div className="login__input">
+              <i className="login__icon">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  width="28"
+                  height="28"
+                  fill="rgba(255,255,255,1)"
+                >
+                  <path d="M18 8H20C20.5523 8 21 8.44772 21 9V21C21 21.5523 20.5523 22 20 22H4C3.44772 22 3 21.5523 3 21V9C3 8.44772 3.44772 8 4 8H6V7C6 3.68629 8.68629 1 12 1C15.3137 1 18 3.68629 18 7V8ZM5 10V20H19V10H5ZM11 14H13V16H11V14ZM7 14H9V16H7V14ZM15 14H17V16H15V14ZM16 8V7C16 4.79086 14.2091 3 12 3C9.79086 3 8 4.79086 8 7V8H16Z"></path>
+                </svg>
+              </i>
+              <input
+                type="password"
+                className="login__input"
+                placeholder="Придумайте пароль"
+              />
+            </div>
+            <div className="login__input">
+              <i className="login__icon">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  width="28"
+                  height="28"
+                  fill="rgba(255,255,255,1)"
+                >
+                  <path d="M6 8V7C6 3.68629 8.68629 1 12 1C15.3137 1 18 3.68629 18 7V8H20C20.5523 8 21 8.44772 21 9V21C21 21.5523 20.5523 22 20 22H4C3.44772 22 3 21.5523 3 21V9C3 8.44772 3.44772 8 4 8H6ZM19 10H5V20H19V10ZM11 15.7324C10.4022 15.3866 10 14.7403 10 14C10 12.8954 10.8954 12 12 12C13.1046 12 14 12.8954 14 14C14 14.7403 13.5978 15.3866 13 15.7324V18H11V15.7324ZM8 8H16V7C16 4.79086 14.2091 3 12 3C9.79086 3 8 4.79086 8 7V8Z"></path>
+                </svg>
+              </i>
+              <input
+                type="password"
+                className="login__input"
+                placeholder="Повторите пароль"
+              />
+            </div>
             <div className="login__button">
               <Link to="/">
                 <button>Зарегистрироваться</button>
