ce80c11ac2b4357c234b657aafa917d971be087b
norma <normanmagic2004@gmail.com>
2025-03-21 15:35:02 +0300

    $

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -13,6 +13,7 @@ export default function Header() {
   const lastScrollPos = useRef(0);
 
   useEffect(() => {
+    
     const handleScroll = () => {
       const currentScrollPos = window.pageYOffset;
 
